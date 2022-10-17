08fbcce60dcac5f812b53795e819610898bc3fa2
YOUR NAME <ameya.charnalia@gmail.com>
2022-10-17 17:06:58 -0400

    Modified navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,12 @@
 import styles from '../styles/Nav.module.css'
+import { GiFoodTruck } from 'react-icons/gi'
 
 const Nav = () => {
     return ( 
         <nav className={styles.nav}>
             <div>
                 <h1>Eat the Strip</h1>
+                <GiFoodTruck className={styles.logo}/>
                 <hr/>
             </div>
         </nav>
