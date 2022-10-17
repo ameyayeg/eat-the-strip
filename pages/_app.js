52c6508ac5aead36e77342c0a83d6905bff93791
YOUR NAME <ameya.charnalia@gmail.com>
2022-10-17 19:09:16 -0400

    Attempt to fix Netlify deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/styles.css'
+import Head from "next/head"
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Eat the Strip</title>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
