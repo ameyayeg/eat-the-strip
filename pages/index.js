fec7d669b4a967b3396089ba923e8b41376d989d
YOUR NAME <ameya.charnalia@gmail.com>
2023-01-28 20:14:13 -0500

    Changed grid

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ const Home = ( {blogs} ) => {
               <div className={styles.thumbnail} key={blog.slug}>
                 <Link href={`/blog/${blog.slug}`}>
                   <a>
-                    <div style={{backgroundImage: `url(${blog.thumbnail})`, backgroundSize: `cover`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', height: '300px', width: '300px', position: 'relative'}}>
+                    <div style={{backgroundImage: `url(${blog.thumbnail})`, backgroundSize: `cover`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', height: '200px', width: '300px', position: 'relative'}}>
                       <span style={{position: 'absolute', backgroundColor: 'yellow', color: 'black', top: '0', left: '0', textTransform: 'uppercase', padding: '0.25rem 0.5rem', fontWeight: 'bold'}}>{blog.cuisine}</span>
                       <span style={{position: 'absolute', backgroundColor: 'black', color: 'white', bottom: '0', left: '0', right: '0', padding: '0.5rem 0.75rem'}}>{blog.title}</span>
                     </div>
