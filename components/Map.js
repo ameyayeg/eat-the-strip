ce9d896ef3d853587dceb5c37abd68250c6c1528
YOUR NAME <ameya.charnalia@gmail.com>
2023-02-10 07:44:22 -0500

    Changed map SSR

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet'
-
-export function ChangeView({ coords }) {
-  const map = useMap()
-  map.setView(coords, 12)
-  return null
-}
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css' // Re-uses images from ~leaflet package
+import * as L from 'leaflet'
+import 'leaflet-defaulticon-compatibility'
 
 export default function Map() {
-  const [geoData, setGeoData] = useState({ lat: 64.536634, lng: 16.779852 })
+  const [geoData, setGeoData] = useState({
+    lat: 45.34299725708286,
+    lng: -75.75570274602492,
+  })
 
   const center = [geoData.lat, geoData.lng]
 
@@ -19,10 +19,11 @@ export default function Map() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {geoData.lat && geoData.lng && (
-        <Marker position={[geoData.lat, geoData.lng]} />
-      )}
-      <ChangeView coords={center} />
+      <Marker position={center}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   )
 }
