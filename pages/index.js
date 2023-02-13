3dcec22c7013b6bc5843eae5dc7b13cbdfc70fcd
YOUR NAME <ameya.charnalia@gmail.com>
2023-02-12 23:43:51 -0500

    Added skeleton

--- pages/index.js
+++ pages/index.js
@@ -3,70 +3,117 @@ import styles from '../styles/Home.module.css'
 import fs from 'fs'
 import matter from 'gray-matter'
 import { BsSearch } from 'react-icons/bs'
-import { Skeleton } from '@mui/material'
 import Search from '../components/Search'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export async function getStaticProps() {
-  const blogFiles= fs.readdirSync('./content/blogs')
+  const blogFiles = fs.readdirSync('./content/blogs')
 
-  const blogs = blogFiles.map(filename => {
-    const file = fs.readFileSync(`./content/blogs/${filename}`, 'utf8')
-    const matterData = matter(file)
+  const blogs = blogFiles
+    .map((filename) => {
+      const file = fs.readFileSync(`./content/blogs/${filename}`, 'utf8')
+      const matterData = matter(file)
 
-    return {
-      ...matterData.data,
-      slug: filename.slice(0, filename.indexOf('.'))
-    }
-  }).sort(function(a,b) {
-    return new Date(b.date) - new Date(a.date);
-  });
+      return {
+        ...matterData.data,
+        slug: filename.slice(0, filename.indexOf('.')),
+      }
+    })
+    .sort(function (a, b) {
+      return new Date(b.date) - new Date(a.date)
+    })
   return {
     props: {
-      blogs
-    }
+      blogs,
+    },
   }
 }
 
-
-const Home = ( {blogs} ) => {
-
-  const [query, setQuery] = useState("")
+const Home = ({ blogs }) => {
+  const [query, setQuery] = useState('')
   const filteredItems = getFilteredItems(query, blogs)
+  const [loading, setLoading] = useState(true)
 
   function getFilteredItems(query, blogs) {
-    if(!query) {
+    if (!query) {
       return blogs
     }
-    return blogs.filter(blog => blog.title.toLowerCase().includes(query)) 
+    return blogs.filter((blog) => blog.title.toLowerCase().includes(query))
   }
 
-  return ( 
+  function Box({ children }) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          padding: '1rem',
+          width: '290px',
+          height: '290px',
+        }}
+      >
+        {children}
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    setLoading(false)
+  }, [blogs])
+
+  return (
     <>
-      <Search 
-        query={query}
-        setQuery={setQuery}
-      />
+      <Search query={query} setQuery={setQuery} />
       <section className={styles.container}>
-        {
-          filteredItems ?         
-            filteredItems.map(blog => (
-              <div className={styles.thumbnail} key={blog.slug}>
-                <Link href={`/blog/${blog.slug}`}>
-                  <a>
-                    <div style={{backgroundImage: `url(${blog.thumbnail})`}}>
-                      <span style={{position: 'absolute', backgroundColor: 'yellow', color: 'black', top: '0', left: '0', textTransform: 'uppercase', padding: '0.25rem 0.5rem', borderRadius: '16px'}}>{blog.cuisine}</span>
-                      <span style={{position: 'absolute', backgroundColor: 'black', color: 'white', bottom: '0', left: '0', right: '0', padding: '0.5rem 0.75rem'}}>{blog.title}</span>
-                    </div>
-                  </a>
-                </Link>
-              </div>
-            )) : 
-              <Skeleton variant="rectangular" width={300} height={300} />
-          }
+        {loading ? (
+          <Skeleton
+            containerClassName={styles.skeleton}
+            wrapper={Box}
+            count={blogs.length}
+          />
+        ) : (
+          filteredItems.map((blog) => (
+            <div className={styles.thumbnail} key={blog.slug}>
+              <Link href={`/blog/${blog.slug}`}>
+                <a>
+                  <div style={{ backgroundImage: `url(${blog.thumbnail})` }}>
+                    <span
+                      style={{
+                        position: 'absolute',
+                        backgroundColor: 'yellow',
+                        color: 'black',
+                        top: '0',
+                        left: '0',
+                        textTransform: 'uppercase',
+                        padding: '0.25rem 0.5rem',
+                        borderRadius: '16px',
+                      }}
+                    >
+                      {blog.cuisine}
+                    </span>
+                    <span
+                      style={{
+                        position: 'absolute',
+                        backgroundColor: 'black',
+                        color: 'white',
+                        bottom: '0',
+                        left: '0',
+                        right: '0',
+                        padding: '0.5rem 0.75rem',
+                      }}
+                    >
+                      {blog.title}
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          ))
+        )}
       </section>
     </>
-   )
+  )
 }
- 
-export default Home;
+
+export default Home
