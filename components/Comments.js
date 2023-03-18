bef9ad1e743090a70b4337a9d874fd896c2f3f4a
YOUR NAME <ameya.charnalia@gmail.com>
2023-03-18 12:19:21 -0400

    Clean up ignored files

--- components/Comments.js
+++ components/Comments.js
@@ -3,7 +3,7 @@ import { DiscussionEmbed } from 'disqus-react'
 const Comments = ({ slug, title }) => {
   return (
     <DiscussionEmbed
-      shortname={process.env.customKey}
+      shortname={process.env.NEXT_PUBLIC_API_KEY}
       config={{
         url: `http://www.eatthestrip.com/blog/${slug}`,
         identifier: slug,
