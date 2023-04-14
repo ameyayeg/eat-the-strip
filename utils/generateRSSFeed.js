40532f1d3bc00c4f67fe54549c407e880cd29781
YOUR NAME <ameya.charnalia@gmail.com>
2023-04-14 14:29:28 -0400

    Modified RSS

--- utils/generateRSSFeed.js
+++ utils/generateRSSFeed.js
@@ -12,7 +12,7 @@ export default async function generateRssFeed() {
     description: `Eat the Strip is our tribute to the mom-and-pop food joints peppered across the Ottawa-Gatineau suburbs. We feel the folks who run these restaurants, diners and bars don't get the credit they deserve for contributing to our city's food culture. So, we created a review blog that only caters to strip mall gems.`,
     site_url: site_url,
     feed_url: `${site_url}/rss.xml`,
-    image_url: `favicon.ico`,
+    image_url: `${site_url}_ipx/w_3840,q_75/%2F_next%2Fstatic%2Fmedia%2Fpexels-makafood-8984408.a837cd84.jpg?url=%2F_next%2Fstatic%2Fmedia%2Fpexels-makafood-8984408.a837cd84.jpg&w=3840&q=75`,
     pubDate: new Date(),
     copyright: `All rights reserved ${new Date().getFullYear()}, Ameya Charnalia`,
   }
@@ -27,7 +27,6 @@ export default async function generateRssFeed() {
       author: post.author,
       lat: post.positives,
       long: post.negatives,
-      enclosure: { url: post.thumbnail },
     })
   })
 
