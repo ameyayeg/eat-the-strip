41a03b1da66e722de6a6f4123e1fd1740b90ed9d
YOUR NAME <ameya.charnalia@gmail.com>
2025-02-04 15:32:58 -0500

    Added donate button

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -10,6 +10,31 @@ const Footer = () => {
   return (
     <footer className={styles.container}>
       <div className={styles.menu}>
+        <form
+          action="https://www.paypal.com/donate"
+          method="post"
+          target="_top"
+        >
+          <input type="hidden" name="business" value="4K2E9APZQPMSN" />
+          <input type="hidden" name="no_recurring" value="0" />
+          <input type="hidden" name="currency_code" value="CAD" />
+          <input
+            type="image"
+            src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+            border="0"
+            name="submit"
+            title="PayPal - The safer, easier way to pay online!"
+            alt="Donate with PayPal button"
+          />
+          <img
+            alt=""
+            border="0"
+            src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
+            width="1"
+            height="1"
+          />
+        </form>
+
         <Link href="/">
           <a>
             <h1>Eat the Strip</h1>
--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -25,6 +25,30 @@ const Layout = ({ children }) => {
         <a id="terms" className="menu-item" href="/terms">
           Terms and conditions
         </a>
+        <form
+          action="https://www.paypal.com/donate"
+          method="post"
+          target="_top"
+        >
+          <input type="hidden" name="business" value="4K2E9APZQPMSN" />
+          <input type="hidden" name="no_recurring" value="0" />
+          <input type="hidden" name="currency_code" value="CAD" />
+          <input
+            type="image"
+            src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+            border="0"
+            name="submit"
+            title="PayPal - The safer, easier way to pay online!"
+            alt="Donate with PayPal button"
+          />
+          <img
+            alt=""
+            border="0"
+            src="https://www.paypal.com/en_CA/i/scr/pixel.gif"
+            width="1"
+            height="1"
+          />
+        </form>
       </Menu>
       <Header />
       <main className={styles.main}>{children}</main>
