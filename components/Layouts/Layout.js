85ec79e5c785ba54b7408b222f8e9a816ceddd1e
YOUR NAME <ameya.charnalia@gmail.com>
2025-02-15 08:56:37 -0500

    Moved the hamburger into the header

--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -1,8 +1,6 @@
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import styles from './Layout.module.css'
-import { slide as Menu } from 'react-burger-menu'
-import { FaInstagram } from 'react-icons/fa'
 
 const Layout = ({ children }) => {
   function showSettings() {
@@ -10,39 +8,6 @@ const Layout = ({ children }) => {
   }
   return (
     <div className={styles.container}>
-      <Menu>
-        <a id="home" className="menu-item" href="/">
-          Home
-        </a>
-        <a id="about" className="menu-item" href="/about">
-          About
-        </a>
-        <a id="all" className="menu-item" href="/all">
-          Restaurants map
-        </a>
-        <a id="privacy" className="menu-item" href="/privacy">
-          Privacy
-        </a>
-        <a id="terms" className="menu-item" href="/terms">
-          Terms and conditions
-        </a>
-        <a href="https://ko-fi.com/B0B31A86OB" target="_blank">
-          <img
-            height="36"
-            style={{ border: '0px', height: '36px' }}
-            src="https://storage.ko-fi.com/cdn/kofi2.png?v=6"
-            border="0"
-            alt="Buy Me a Coffee at ko-fi.com"
-          />
-        </a>
-        <a
-          href="https://www.instagram.com/eatthestripyow/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <FaInstagram color="#ee802f" />
-        </a>
-      </Menu>
       <Header />
       <main className={styles.main}>{children}</main>
       <Footer />
