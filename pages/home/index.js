62502eb7cbdde540fe457763727fe42a79bd407f
YOUR NAME <ameya.charnalia@gmail.com>
2025-08-22 22:59:16 -0300

    Removing near me option

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,9 +2,7 @@ import Link from 'next/link'
 import styles from './Home.module.css'
 import Search from '../../components/Search/Search'
 import { useState } from 'react'
-import { ImLocation } from 'react-icons/im'
 import { useEffect } from 'react'
-import haversineDistance from '../../utils/haversine'
 import { getSortedPosts } from '../../utils/mdx'
 import generateRssFeed from '../../utils/generateRSSFeed'
 import Image from 'next/image'
@@ -24,14 +22,12 @@ const Home = ({ blogs }) => {
   const [userCoordinates, setUserCoordinates] = useState(null)
   const [allRestaurants, setAllRestaurants] = useState(blogs)
   const [status, setStatus] = useState('loading')
-  const [isLocationsActivated, setIsLocationActivated] = useState(false)
   const [city, setCity] = useState('Ottawa') // default
 
   useEffect(() => {
     if (!query) {
       setAllRestaurants(blogs)
       setStatus('loaded')
-      setIsLocationActivated(false)
     } else {
       const searchedRestaurants = allRestaurants.filter((restaurant) => {
         return restaurant.cuisine.toLowerCase().includes(query)
@@ -45,56 +41,10 @@ const Home = ({ blogs }) => {
     }
   }, [query])
 
-  function handleLocation() {
-    if (userCoordinates) {
-      const filteredByDistance = allRestaurants.filter((restaurant) => {
-        const distance = haversineDistance(userCoordinates, [
-          restaurant.positives,
-          restaurant.negatives,
-        ])
-        if (distance < 1) {
-          return true
-        }
-      })
-      if (filteredByDistance.length < 1) {
-        setStatus('none')
-      } else {
-        setAllRestaurants(filteredByDistance)
-        if (!isLocationsActivated) {
-          setIsLocationActivated(true)
-        } else {
-          setAllRestaurants(blogs)
-
-          setIsLocationActivated(false)
-        }
-      }
-    }
-  }
-
-  useEffect(() => {
-    if (navigator?.geolocation) {
-      navigator.geolocation.getCurrentPosition((location) => {
-        if (location)
-          setUserCoordinates([
-            location.coords.latitude,
-            location.coords.longitude,
-          ])
-      })
-    }
-  }, [status])
-
   function resetRestaurants() {
     setAllRestaurants(blogs)
     setStatus('loaded')
     setQuery('')
-  }
-
-  function distanceCalculator(restaurantCoordinates) {
-    const decimalNumber = haversineDistance(
-      userCoordinates,
-      restaurantCoordinates
-    )
-    return `${Math.ceil(decimalNumber)} km`
   }
 
   function handleCityToggle() {
@@ -109,18 +59,7 @@ const Home = ({ blogs }) => {
   return (
     <>
       <Search query={query} setQuery={setQuery} />
-      {userCoordinates && (
-        <ImLocation
-          style={{
-            fontSize: '2rem',
-            position: 'absolute',
-            top: '36px',
-            right: '36px',
-            cursor: 'pointer',
-          }}
-          onClick={handleLocation}
-        />
-      )}
+
       <div
         style={{
           marginBottom: '1rem',
@@ -204,22 +143,7 @@ const Home = ({ blogs }) => {
                       Closed
                     </span>
                   )}
-                  {isLocationsActivated && (
-                    <span
-                      style={{
-                        position: 'absolute',
-                        backgroundColor: 'yellow',
-                        color: 'black',
-                        top: '0',
-                        right: '0',
-                        textTransform: 'uppercase',
-                        padding: '0.25rem 0.5rem',
-                        borderRadius: '16px',
-                      }}
-                    >
-                      &lt; 1km
-                    </span>
-                  )}
+
                   <span
                     style={{
                       position: 'absolute',
